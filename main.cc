57d875af0bd15a355235dd2cd0b41cc45a1272a4
pixel <pixel@pixel-PC.lan>
2016-03-06 22:17:04 -0500

    Added a check if there is no input file (will complain and exit with error code 1) Added cmake exess files in the .gitignore Added the -g parameter to the compiler for gnu debugging (remove for production builds)

--- main.cc
+++ main.cc
@@ -7,6 +7,10 @@ using namespace Luna;
 typedef void (*AssembledFunction)();
 
 int main(int argc, char** argv){
+    if (argc < 2) {
+    	printf("No input file.");
+    	return 1;
+    }
     Scope* global = new Scope();
     Luna::Internal::InitOnce(global);
     FILE* fin = fopen(argv[1], "r");
